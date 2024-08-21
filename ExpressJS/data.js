6c3148a6ee995fe9740e64a5e1ef736fcdddc120
KC1064 <kiron08072003@gmail.com>
2024-08-21 21:40:49 +0530

    Express JSON

---
+++ ExpressJS/data.js
@@ -0,0 +1,19 @@
+const phone = [{
+    "id": 1,
+    "name": "iPhone 13 Pro",
+    "price": 3000,
+},{
+    "id": 2,
+    "name": "Oppo V25 Pro",
+    "price": 2500,
+},{
+    "id": 3,
+    "name": "OnePlus Nord",
+    "price": 2299,
+},{
+    "id": 4,
+    "name": "Redmi 12 Pro Max",
+    "price": 1999,
+}]
+
+module.exports = phone;
---
+++ ExpressJS/expressJson.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const phone = require('./data'); 
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1> <a href="/phones">Phones</a> <a href="/sale">Sales</a>');
+});
+
+
+app.get('/phones', (req, res) => {
+    res.json(phone);
+});
+
+app.get('/sale',(req,res)=>{
+    const newItems = phone.map((item)=>{
+        const {name,price} = item;
+        return {name,price};
+    })
+    res.json(newItems);
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+
+app.get('/phones/:id', (req, res) => {
+    console.log(req.params);
+    const phoneID = parseInt(req.params.id); 
+    const phone_data = phone.find(item => item.id === phoneID);
+    if (phone_data) {
+        res.json(phone_data);
+    } else {
+        res.status(404).json({ error: 'Phone not found' });
+    }
+});
